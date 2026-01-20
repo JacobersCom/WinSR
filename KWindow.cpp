50d2f78e7bbd92752d421d3e33f12979f616c430
JacobersCom <jacobymartin1@icloud.com>
2026-01-20 12:18:40 -0500

    Create win32Window function in the works. Still working on namespaces

--- KWindow.cpp
+++ KWindow.cpp
@@ -2,20 +2,64 @@
 
 #include <windows.h>
 
-namespace KE::SYSTEM::KWindow
+namespace KE::SYSTEM
 {
 	KReturn KWindow::Create()
 	{
 		//Creates the window
+		CreateWin32Window();
 
-		HINSTANCE WindowInstance = GetModuleHandle(NULL);
-
-		STARTUPINFO StartUpInfo;
-		GetStartupInfo(&StartUpInfo);
 	}
 
 	KReturn KWindow::SetWindowTitle(const wchar_t title)
 	{
 		//Sets window title 
+	}
+	
+	KReturn KWindow::CreateWin32Window(WindowDesc& Desc)
+	{
+		PtrLoader = GetModuleHandle(NULL);
+
+
+
+		WNDCLASS wc = {};
+		wc.hInstance = PtrLoader;
+		wc.lpszClassName = reinterpret_cast<LPCWSTR>(Desc.ClassName);
+		wc.lpfnWndProc = 
+	}
+	
+	LRESULT KWindow::WindowProc(HWND Window, UINT message, WPARAM wParam, LPARAM lParam)
+	{
+		switch (message)
+		{
+		case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+		}break;
+		case WM_CLOSE:
+		{
+			DestroyWindow(Window);
+		}break;
+		case WM_SIZE:
+		{
+			RECT ClientRect;
+			GetClientRect(Window, &ClientRect);
+			int width = ClientRect.right - ClientRect.left;
+			int height = ClientRect.bottom - ClientRect.top;
+			break;
+		}
+		case WM_PAINT:
+		{
+
+			PAINTSTRUCT Paint;
+			HDC DeviceContext = BeginPaint(Window, &Paint);
+			RECT ClientRECT;
+			GetClientRect(Window, &ClientRECT);
+			//EndPaint(hwnd, &Paint);
+			break;
+		}
+		}
+
+		return DefWindowProc(Window, message, wParam, lParam);
 	}
 }
--- KWindow.h
+++ KWindow.h
@@ -1,19 +1,40 @@
 #include "KReturn.h"
 
 //Namespace holds data for making the window
-namespace KE
+namespace KE::SYSTEM
 {
 
-	namespace SYSTEM
+	struct WindowDesc
 	{
-		
-	}
-}
+		const wchar_t ClassName;
+		const int WindowWidth;
+		const int WindowHeight;
 
-//Class holds functions and private memebers for class
-class KWindow 
-{
-public:
+	};
+	
 
-	KReturn Create();
+
+	//Class holds functions and private memebers for class
+	class KWindow
+	{
+	public:
+
+		KReturn Create();
+		KReturn SetWindowTitle(const wchar_t Title);
+
+	private:
+
+		KReturn CreateWin32Window(WindowDesc& desc);
+
+		static LRESULT WindowProc(HWND Window, UINT message, WPARAM wParam, LPARAM lParam);
+
+	private:
+
+		//ptr to where the DLL is held
+		HINSTANCE PtrLoader = nullptr;
+
+		//Handle to the window
+		HWND WindowHandle = nullptr;
+	};
+
 };
