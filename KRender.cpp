a29182be2caf94132968f5fd02824ae0bf581611
JacobersCom <jacobymartin1@icloud.com>
2026-02-06 16:19:32 -0500

    Code clean up

--- KRender.cpp
+++ KRender.cpp
@@ -13,7 +13,6 @@ namespace KE::SYSTEM
 	KReturn KRender::InitVulkan() 
 	{
 		KE::VULKAN::CreateVkInstance(_VkInstance);
-		KE::VULKAN::SetUpDebuggerMessage();
 		KE::VULKAN::CreateWin32Surface(_win.GetWindowHandle(), _win.GetWindowInstance(), _VkInstance, _VkSurface);
 		KE::VULKAN::PickPhyicalDevice(_VkPhyscialDevice, _VkInstance);
 		KE::VULKAN::FindQueueFamilies(_VkPhyscialDevice);
