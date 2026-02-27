3a094c570349e49ddb30c8248ea31606129e9f03
JacoberCom <jacobymartin1@icloud.com>
2026-02-26 22:57:52 -0500

    Removed testing from main.

--- main.cpp
+++ main.cpp
@@ -9,10 +9,6 @@ int main()
 {
 	KE::SYSTEM::KWindow _win;
 	KE::RENDERER::KRender _render(_win);
-	KE::KOMATH::Vec3D _vec3(1.0f, 1.0f, 1.0f);
-	std::cout << _vec3;
-	_vec3 += 1.0;
-	std::cout << _vec3;
 
 	_win.SetWindowDesc(L"KOS", L"KOS Engine", 500, 500);
 	_win.Create();
