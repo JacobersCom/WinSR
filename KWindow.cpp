f424b2578992e4e38bc86860fa3fdcc97b0c655c
JacoberCom <jacobymartin1@icloud.com>
2026-02-27 12:44:26 -0500

    Custom FrameBuffer function for bufferSize

--- KWindow.cpp
+++ KWindow.cpp
@@ -24,6 +24,34 @@ namespace KE::SYSTEM
 
 	
 	}
+
+	void KWindow::GetFrameBufferSize(HWND WindowHandle, int& Width, int& Height)
+	{
+		RECT ClientRect;
+		GetClientRect(WindowHandle, &ClientRect);
+
+		//Top left screen coord
+		POINT p0{ ClientRect.left, ClientRect.top };
+		//Bottom right screen coord
+		POINT p1{ ClientRect.right, ClientRect.bottom };
+
+		using WinFp = BOOL(WINAPI*)(HWND, LPPOINT);
+
+		HMODULE User32 = GetModuleHandleW(L"User32.dll");
+
+		//Returns address of function else returns nullptr. 
+		auto logicalToPhysicalPM = (WinFp)GetProcAddress(User32, "LogicalToPhysicalPointForPerMonitorDPI");
+
+		if (logicalToPhysicalPM)
+		{
+			logicalToPhysicalPM(WindowHandle, &p0);
+			logicalToPhysicalPM(WindowHandle, &p1);
+
+		}
+
+		Width = (int)(p1.x - p0.x);
+		Height = (int)(p1.y - p0.y);
+	}
 	
 	KReturn KWindow::CreateWin32Window(WindowDesc& Desc)
 	{
